bedd8a7d4eaf781656929ab8e0f460df3c0b819b
Jorn <Lotsch>
2022-01-14 19:45:43 +0100

    Code work

--- box_and_heatplots.py
+++ box_and_heatplots.py
@@ -6,20 +6,35 @@ Created on Wed Jan 12 12:05:18 2022
 @author: joern
 """
 
-def box_and_heatplot(data, title="None"):
-    
-    import seaborn as sns
-    import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import pandas as pd
+
+
+def box_and_heatplot(data, title=None, scale=False, **kwargs):
 
     fig, axes = plt.subplots(1, 2, figsize=(20, 20))
-    fig.suptitle("Groups of variables: " + title)
-    sns.heatmap(ax=axes[0], data = data)
-    ax = sns.violinplot(ax=axes[1], data = data, saturation = 0.5)
-    ax.set_xticklabels(ax.get_xticklabels(),rotation = 90)
-    plt.setp(ax.collections, alpha=.3)
-    ax = sns.boxplot(ax=axes[1], data = data, fliersize = 0,  width=0.2)
-    plt.setp(ax.collections, alpha=.5)
-    sns.swarmplot(ax=axes[1], data = data)
+    Title = ("Groups of variables: " +
+             title) if title != None else "Variables"
+    fig.suptitle(Title)
+    if len(data.columns) > 0:
+        if scale:
+            min_max_scaler = preprocessing.MinMaxScaler()
+            data_scaled = min_max_scaler.fit_transform(data)
+            data_scaled = pd.DataFrame(data_scaled, columns=data.columns)
+            ax = sns.heatmap(ax=axes[0], data=data_scaled, **kwargs)
+        else:
+            ax = sns.heatmap(ax=axes[0], data=data, **kwargs)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+        ax = sns.violinplot(ax=axes[1], data=data, saturation=0.5)
+        ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+        plt.setp(ax.collections, alpha=.3)
+        ax = sns.boxplot(ax=axes[1], data=data, fliersize=0,  width=0.2)
+        plt.setp(ax.collections, alpha=.5)
+        sns.swarmplot(ax=axes[1], data=data)
+    else:
+        ax = fig.add_subplot(1, 2, 1)
+        ax.text(0.5, 0.5, "No data", fontsize=24)
 
     return(fig)
-    
--- explore_tukey_lop_6.py
+++ explore_tukey_lop_6.py
@@ -6,11 +6,21 @@ Created on Mon Jan 10 18:47:48 2022
 @author: joern
 """
 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+from scipy.stats import kstest, normaltest, zscore
+from pandas.api.types import is_numeric_dtype
 
-def explore_tukey_lop(data, powers=[-2, -1, -0.5, 0, 0.5, 1, 2], normtest = "K^2 test"):
+
+def explore_tukey_lop(data, powers=[-2, -1, -0.5, 0, 0.5, 1, 2], normtest="K^2 test", outlierremoval = False):
 
     powersDict = {"reciprocal square": -2, "reciprocal": -1, "reciprocal square root": -
                   0.5, "log": 0, "square root": 0.5, "none": 1, "square": 2}
+
+    # %% Check some input parameters
     if powers:
         if not all(x in powersDict.values() for x in powers):
             raise Warning("Input does not macth Tukey's powers!")
@@ -20,20 +30,13 @@ def explore_tukey_lop(data, powers=[-2, -1, -0.5, 0, 0.5, 1, 2], normtest = "K^2
                       if value in powers}
     else:
         powers = powersDict
-        
-    if not normtest in ["K^2 test", "KS test"]:
-        raise Warning("For normality testing,  D’Agostino’s K^2 test and KS tests are implemented! Normailty test set to  D’Agostino’s K^2 test.")
-        normtest  = "K^2 test"
-             
-    import numpy as np
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-    from sklearn import preprocessing
-    from sklearn.linear_model import LinearRegression
-    from scipy.stats import kstest
-    from scipy.stats import normaltest
-    from pandas.api.types import is_numeric_dtype
 
+    if not normtest in ["K^2 test", "KS test"]:
+        raise Warning(
+            "For normality testing, D’Agostino’s K^2 test and the KS test are implemented! Normailty test set to D’Agostino’s K^2 test.")
+        normtest = "K^2 test"
+
+    # %% Subfunction definitions
     def data_transform(data, tk_power):
         if tk_power != 0:
             a = np.power(data.astype("float"), tk_power)
@@ -56,36 +59,61 @@ def explore_tukey_lop(data, powers=[-2, -1, -0.5, 0, 0.5, 1, 2], normtest = "K^2
         predictions = model.predict(X)
         qq[:, 2] = predictions
         return(qq)
-    
-    if  is_numeric_dtype(data):
-        data = data[~np.isnan(data)]
-        fig, axes = plt.subplots(len(powers.keys()), 3, figsize=(20, 5*len(powers.keys())))
-        for i, actual_power in enumerate(list(powers.values())):
-            data_transformed = data_transform(data=data, tk_power=actual_power)
-            data_transformed = data_transformed[~np.isnan(data_transformed)]
-            data_transformed = data_transformed[~np.isinf(data_transformed)]
-            
-            if normtest == "KS test":
-                data_transformed_standardized = preprocessing.scale(data_transformed)
-                normaltest_result = kstest(data_transformed_standardized, "norm")
-                normaltest_result_p = str("{:.3E}".format(normaltest_result.pvalue))
-            else:
-                normaltest_result = normaltest(data_transformed)
-                normaltest_result_p = str("{:.3E}".format(normaltest_result.pvalue))
-                
-            qq_values = qq_createValues(data = data_transformed)
-            if len(powers.keys()) > 1:
-                sns.histplot(ax=axes[i, 0], x=data_transformed).set(title="Histogram: Data = " + data.name)
-                sns.distplot(ax=axes[i, 1], x=data_transformed, hist=False).set(title="pdf: transformation = " + list(powers.keys())[i])
-                sns.scatterplot(ax=axes[i, 2], x=qq_values[:, 0], y=qq_values[:, 1]).set(title="QQ plot, " + normtest + " p-value = " + normaltest_result_p)
-                sns.lineplot(ax=axes[i, 2], x=qq_values[:, 0], y=qq_values[:, 2], color="salmon")
-            else:
-                sns.histplot(ax=axes[0], x=data_transformed).set(title="Histogram: Data = " + data.name)
-                sns.distplot(ax=axes[1], x=data_transformed, hist=False).set(title="pdf: transformation = " + list(powers.keys())[i])
-                sns.scatterplot(ax=axes[2], x=qq_values[:, 0], y=qq_values[:, 1]).set(title="QQ plot, " + normtest + " p-value = " + normaltest_result_p)
-                sns.lineplot(ax=axes[2], x=qq_values[:, 0], y=qq_values[:, 2], color="salmon")
-            plt.xlabel("Normal theoretical quantiles")
-            plt.ylabel("Ordered data")
-        return(fig)
-    else: 
+
+    # %% Main function
+    if not is_numeric_dtype(data):
+        raise Warning("Data is not numeric")
         return
+    else:
+        data = data.dropna(axis = 0)
+    
+    if outlierremoval:
+        data = data.dropna(axis = 0)
+        z = np.abs(zscore(data))
+        data = data[(z < 3)]
+   
+    if len(data) < 2:
+       raise Warning("Too few data")
+       return
+    figColors = ["salmon" if x==1 else "Dodgerblue" for x in powers.values()]
+    fig, axes = plt.subplots(
+        len(powers.keys()), 3, figsize=(20, 5*len(powers.keys())))
+    for i, actual_power in enumerate(list(powers.values())):
+        data_transformed = data_transform(data=data, tk_power=actual_power)
+        data_transformed = data_transformed[~np.isnan(data_transformed)]
+        data_transformed = data_transformed[~np.isinf(data_transformed)]
+
+        if normtest == "KS test":
+            data_transformed_standardized = preprocessing.scale(
+                data_transformed)
+            normaltest_result = kstest(
+                data_transformed_standardized, "norm")
+            normaltest_result_p = str(
+                "{:.3E}".format(normaltest_result.pvalue))
+        else:
+            normaltest_result = normaltest(data_transformed)
+            normaltest_result_p = str(
+                "{:.3E}".format(normaltest_result.pvalue))
+
+        qq_values = qq_createValues(data=data_transformed)
+        if len(powers.keys()) > 1:
+            sns.histplot(ax=axes[i, 0], x=data_transformed, color=(figColors[i])).set(
+                title="Histogram: Data = " + data.name)
+            sns.distplot(ax=axes[i, 1], x=data_transformed, hist=False, color=(figColors[i])).set(
+                title="pdf: transformation = " + list(powers.keys())[i])
+            sns.scatterplot(ax=axes[i, 2], x=qq_values[:, 0], y=qq_values[:, 1], color=(figColors[i])).set(
+                title="QQ plot, " + normtest + " p-value = " + normaltest_result_p)
+            sns.lineplot(ax=axes[i, 2], x=qq_values[:, 0],
+                         y=qq_values[:, 2], color="grey")
+        else:
+            sns.histplot(ax=axes[0], x=data_transformed, color=(figColors[i])).set(
+                title="Histogram: Data = " + data.name)
+            sns.distplot(ax=axes[1], x=data_transformed, hist=False, color=(figColors[i])).set(
+                title="pdf: transformation = " + list(powers.keys())[i])
+            sns.scatterplot(ax=axes[2], x=qq_values[:, 0], y=qq_values[:, 1], color=(figColors[i])).set(
+                title="QQ plot, " + normtest + " p-value = " + normaltest_result_p)
+            sns.lineplot(ax=axes[2], x=qq_values[:, 0],
+                         y=qq_values[:, 2], color="grey")
+        plt.xlabel("Normal theoretical quantiles")
+        plt.ylabel("Ordered data")
+    return(fig)
